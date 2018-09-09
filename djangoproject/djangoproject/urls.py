58f57b190da5d11773352514ccb3a25b8a25de6f
GoldenChicken <paveltikhomirov41@gmail.com>
2018-09-09 01:09:55 -0700

    push

---
+++ djangoproject/djangoproject/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.conf.urls import url,include
+
+urlpatterns = [
+    url(r'^$', include('posts.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^posts/', include('posts.urls')),
+]
